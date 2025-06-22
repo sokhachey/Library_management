38acb07f399bf37545109c61650fac9ae3d7b4f2
Sokha Sim <sokha.sim@student.passerellesnumeriques.org>
2025-06-22 12:05:32 +0700

    added file

---
+++ database/migrations/2025_06_22_045241_create_reports_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('joined_date');
+            $table->timestamp('exits_date')->nullable(); // exit might not be set yet
+            $table->foreignId('admin_id')
+                  ->constrained('admins')
+                  ->onDelete('cascade')
+                  ->onUpdate('cascade');
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->timestamps(); // optional
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reports');
+    }
+};
